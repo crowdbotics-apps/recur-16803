061e7c2a6acee613bf23475ac4dd65b3f1448b1e
Crowdbotics <team@crowdbotics.com>
2020-05-09 19:35:13 +0000

    Update models

--- recur_16803/wsgi.py
+++ recur_16803/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recur_16803.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recur_16803.settings")
 
 application = get_wsgi_application()
